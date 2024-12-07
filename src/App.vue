78b30da0d1eae918e07f96cbf2c64aa6f05688b8
kamiya-kei <miyahei38@gmail.com>
2024-12-07 15:05:25 +0900

    chore: prettier実行

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
 import Members from './components/Members.vue'
-
 </script>
 
 <template>
-        <h1>ホロライブメンバー非公式まとめ</h1>
-        <Members />
-        <div style="margin-top: 200px;"></div>
+  <h1>ホロライブメンバー非公式まとめ</h1>
+  <Members />
+  <div style="margin-top: 200px"></div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
