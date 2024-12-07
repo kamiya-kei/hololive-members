ed0d2552f8cc5ac107ec4ece72fc76dd5889845b
kamiya-kei <miyahei38@gmail.com>
2024-12-07 17:10:52 +0900

    refactor: vTubersのデータをbuildする処理をfunctionsに分離

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref, defineModel, onMounted, watch } from 'vue'
-import { vTubers as vTubersData } from './constants'
 import {
   loadSortTypeConfig,
   TSortType,
@@ -13,6 +12,7 @@ import {
   companyTexts,
   companies,
 } from './functions/storages'
+import { VTuberData, buildVTubers } from './functions/buildVTubers'
 
 const { openLinkMode } = defineProps<{
   openLinkMode: '_blank' | '_self'
@@ -26,115 +26,14 @@ const displayCompanies = defineModel<TCompany[]>('displayCompanies', {
   default: loadCompanyConfig(),
 })
 
-type VTuberData = (typeof vTubersData)[number] & {
-  msg: string
-  msg2: string
-  msg0: string
-  class: string
-}
-
 const vTubers = ref<VTuberData[]>([])
 
-const extraction_date = (s: string) => s.split('-').slice(1).join('/')
-
-const sortGroup = () => {
-  const newVTubers: VTuberData[] = vTubersData.map((v) => ({
-    ...v,
-    msg: ((s) => `${s[0]}年${s[1]}月${s[2]}日デビュー`)(
-      v.debut.split(' ')[0].split('-')
-    ),
-    msg2: ((s) => `誕生日:${s[0]}月${s[1]}日`)(v.birthday.split('/')),
-    msg0: v.group,
-    class: (v.sort / 0.1) % 1 == 0 ? 'cb' : '',
-  }))
-  newVTubers.sort((a, b) => a.sort - b.sort)
-  vTubers.value = newVTubers
-}
-const sortDebut = () => {
-  const newVTubers: VTuberData[] = vTubersData.map((v) => ({
-    ...v,
-    msg: ((s) => `${s[0]}年${s[1]}月${s[2]}日デビュー`)(
-      v.debut.split(' ')[0].split('-')
-    ),
-    msg2: ((s) => `誕生日:${s[0]}月${s[1]}日`)(v.birthday.split('/')),
-    msg0: v.group,
-    class: '',
-  }))
-  newVTubers.sort(
-    (a, b) => new Date(a.debut).getTime() - new Date(b.debut).getTime()
-  )
-  vTubers.value = newVTubers
-}
-const sortDebutDate = () => {
-  const newVTubers: VTuberData[] = vTubersData.map((v) => ({
-    ...v,
-    msg: ((s) => `${s[1]}月${s[2]}日デビュー`)(
-      v.debut.split(' ')[0].split('-')
-    ),
-    msg2: ((s) => `誕生日:${s[0]}月${s[1]}日`)(v.birthday.split('/')),
-    msg0: v.group,
-    class: '',
-  }))
-  newVTubers.sort(
-    (a, b) =>
-      new Date(extraction_date(a.debut)).getTime() -
-      new Date(extraction_date(b.debut)).getTime()
-  )
-  vTubers.value = newVTubers
-}
-const sortBirthday = () => {
-  const newVTubers: VTuberData[] = vTubersData.map((v) => ({
-    ...v,
-    msg: ((s) => `誕生日:${s[0]}月${s[1]}日`)(v.birthday.split('/')),
-    msg2: '',
-    msg0: v.group,
-    class: '',
-  }))
-  newVTubers.sort(
-    (a, b) => new Date(a.birthday).getTime() - new Date(b.birthday).getTime()
-  )
-  vTubers.value = newVTubers
-}
-const sortHeight = () => {
-  const newVTubers: VTuberData[] = vTubersData.map((v) => ({
-    ...v,
-    msg: `${v.height}cm`,
-    msg2: '',
-    msg0: v.group,
-    class: '',
-  }))
-  newVTubers.sort((a, b) => a.height - b.height)
-  vTubers.value = newVTubers
-}
-
-const sortVTubers = (sortType: TSortType) => {
-  switch (sortType) {
-    case 'group':
-      sortGroup()
-      break
-    case 'debut':
-      sortDebut()
-      break
-    case 'debutDate':
-      sortDebutDate()
-      break
-    case 'birthday':
-      sortBirthday()
-      break
-    case 'height':
-      sortHeight()
-      break
-    default:
-      throw new Error('invalid sortType')
-  }
-}
-
 onMounted(async () => {
-  sortVTubers(sortType.value)
+  vTubers.value = buildVTubers(sortType.value)
 })
 
 watch(sortType, (newSortType) => {
-  sortVTubers(newSortType)
+  vTubers.value = buildVTubers(newSortType)
   updateSortTypeConfig(newSortType)
 })
 
---
+++ src/components/functions/buildVTubers.ts
@@ -0,0 +1,102 @@
+import { vTubers as vTubersData } from '../constants'
+import { TSortType } from './storages'
+
+export type VTuberData = (typeof vTubersData)[number] & {
+  msg: string
+  msg2: string
+  msg0: string
+  class: string
+}
+
+const extraction_date = (s: string) => s.split('-').slice(1).join('/')
+
+const buildBySortGroup = (): VTuberData[] => {
+  const newVTubers: VTuberData[] = vTubersData.map((v) => ({
+    ...v,
+    msg: ((s) => `${s[0]}年${s[1]}月${s[2]}日デビュー`)(
+      v.debut.split(' ')[0].split('-')
+    ),
+    msg2: ((s) => `誕生日:${s[0]}月${s[1]}日`)(v.birthday.split('/')),
+    msg0: v.group,
+    class: (v.sort / 0.1) % 1 == 0 ? 'cb' : '',
+  }))
+  newVTubers.sort((a, b) => a.sort - b.sort)
+  return newVTubers
+}
+
+const buildBySortDebut = (): VTuberData[] => {
+  const newVTubers: VTuberData[] = vTubersData.map((v) => ({
+    ...v,
+    msg: ((s) => `${s[0]}年${s[1]}月${s[2]}日デビュー`)(
+      v.debut.split(' ')[0].split('-')
+    ),
+    msg2: ((s) => `誕生日:${s[0]}月${s[1]}日`)(v.birthday.split('/')),
+    msg0: v.group,
+    class: '',
+  }))
+  newVTubers.sort(
+    (a, b) => new Date(a.debut).getTime() - new Date(b.debut).getTime()
+  )
+  return newVTubers
+}
+
+const buildBySortDebutDate = (): VTuberData[] => {
+  const newVTubers: VTuberData[] = vTubersData.map((v) => ({
+    ...v,
+    msg: ((s) => `${s[1]}月${s[2]}日デビュー`)(
+      v.debut.split(' ')[0].split('-')
+    ),
+    msg2: ((s) => `誕生日:${s[0]}月${s[1]}日`)(v.birthday.split('/')),
+    msg0: v.group,
+    class: '',
+  }))
+  newVTubers.sort(
+    (a, b) =>
+      new Date(extraction_date(a.debut)).getTime() -
+      new Date(extraction_date(b.debut)).getTime()
+  )
+  return newVTubers
+}
+
+const buildBySortBirthday = (): VTuberData[] => {
+  const newVTubers: VTuberData[] = vTubersData.map((v) => ({
+    ...v,
+    msg: ((s) => `誕生日:${s[0]}月${s[1]}日`)(v.birthday.split('/')),
+    msg2: '',
+    msg0: v.group,
+    class: '',
+  }))
+  newVTubers.sort(
+    (a, b) => new Date(a.birthday).getTime() - new Date(b.birthday).getTime()
+  )
+  return newVTubers
+}
+
+const buildBySortHeight = (): VTuberData[] => {
+  const newVTubers: VTuberData[] = vTubersData.map((v) => ({
+    ...v,
+    msg: `${v.height}cm`,
+    msg2: '',
+    msg0: v.group,
+    class: '',
+  }))
+  newVTubers.sort((a, b) => a.height - b.height)
+  return newVTubers
+}
+
+export const buildVTubers = (sortType: TSortType): VTuberData[] => {
+  switch (sortType) {
+    case 'group':
+      return buildBySortGroup()
+    case 'debut':
+      return buildBySortDebut()
+    case 'debutDate':
+      return buildBySortDebutDate()
+    case 'birthday':
+      return buildBySortBirthday()
+    case 'height':
+      return buildBySortHeight()
+    default:
+      throw new Error('invalid sortType')
+  }
+}
