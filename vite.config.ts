13bbd705661b24e31760b7e0a2f4ae4c4f00b7cc
kamiya-kei <miyahei38@gmail.com>
2024-12-08 13:16:47 +0900

    chore: 相対パスの設定
    editorconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
